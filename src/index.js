2b27e12e80a99932df24e06768bec7ff01273df2
--replace-all <jonathonsmcclendon@gmail.com>
2025-10-14 13:50:22 -0500

    clean up and tetting up for first build

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import "./initFields";
+
+// Export everything you want users to access
+export { ReturnFieldsV2 } from "./returnFields";
+export { createFormHandler } from "./handlers/createFormHandler";
+export { registerField, getField, getAllFields } from "./core/fieldRegistry";
